9ea62c1c56f55136af6d88d6caa9dec73112eb8d
Hacktiv8 <hacktiv8-11@Hacktiv8-11.local>
2017-04-25 20:26:01 +0700

    rekursi tantangan roket

--- number_in_words.js
+++ number_in_words.js
@@ -2,22 +2,25 @@ function numberToWords(number) {
   // Your code here
   var bilangan = ['nol', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas']
   var hasil = [];
-
+  var penyebut = ['belas', 'puluh', 'ratus', 'ribu', 'puluh', 'ratus', 'juta', 'puluh', 'ratus', 'milyar', 'puluh', 'ratus', 'triliun', 'puluh', 'ratus']
+  var count = 1;
 
 function penyebutAngka(num) {
-  if (2 > num/10 >= 1) {
+  if (num/10 < 1.2) {
+    count++
+    hasil.push(bilangan[num]);
+  } else if (num/10 >= 1.2 && num/10 < 2) {
+    count++
     hasil.push(bilangan[num%10]);
     hasil.push('belas');
-  } else if (num/10 >= 2 && num/10 < 10 ) {
-    hasil.push(bilangan[Math.floor(num/10)]);
-    hasil.push('puluh');
-    hasil.push(bilangan[num%10]);
-  } else if (num/10 >= 10 && num/10 < 100) {
-    hasil.push(bilangan[Math.floor(num/100)])
-    hasil.push('ratus');
-    return penyebutAngka(num%100)
+
+  } else if (num/10 >= 2) {
+    count++;
+    return penyebutAngka(num/10);
   }
-  return hasil.join(' ')
+
+  var angStr = num.toString().split('')
+  return angStr;
 }
 
 
@@ -31,7 +34,8 @@ function penyebutAngka(num) {
 }
 
 // Driver code
-console.log(numberToWords(717));
+console.log(numberToWords(124586812));
+
 
 module.exports = {
   numberToWords: numberToWords
