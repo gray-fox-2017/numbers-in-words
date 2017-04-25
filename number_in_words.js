1b266a71f3773538c103e4bfa155cc34a544bf7d
ambodalle <ambodalle.st@gmail.com>
2017-04-25 20:03:26 +0700

    save dulu

--- number_in_words.js
+++ number_in_words.js
@@ -1,8 +1,32 @@
 function numberToWords(number) {
   // Your code here
+  angka = ["","Satu", "Dua" , " Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"];
+  result = "";
+  if (number < 12) {
+    result += "" + angka(number);
+  } else if(number < 20) {
+    result += numberToWords(number - 10) + Belas
+  } else if(number < 100) {
+    result += numberToWords(Math.floor(number / 10)) + Puluh + numberToWords(number % 10);
+  } else if(number < 200) {
+    result += "Seratus" + numberToWords(number-100);
+  } else if(number < 1000) {
+    result += numberToWords(Math.floor(number/100)) + Ratus + numberToWords(number % 100);
+  } else if() {
+
+  } else if() {
+
+  }else if() {
+
+  }else if() {
+
+  }else if() {
+
+  }
 }
 
 // Driver code
+console.log(numberToWords(23));
 console.log(numberToWords(1000000));
 
 module.exports = {
