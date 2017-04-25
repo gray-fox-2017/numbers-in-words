c6357cd23688d86b995f56c7a0c4c5ab6a4f4bca
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-04-25 18:50:42 +0700

    rili 0 dan rilis 1

--- number_in_words.js
+++ number_in_words.js
@@ -1,9 +1,26 @@
 function numberToWords(number) {
   // Your code here
+  const word = ["","Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"];
+  let result = "";
+  if (number === 0 ){
+    return result;
+  } else if (number < 12){
+    result = result + word[number] + numberToWords(number - number);
+    number = number - number;
+  } else if (number > 11 && number < 20){
+    result = word[(number-10)] + " Belas " + numberToWords(number - number);
+  } else if (number > 20 && number < 100){
+    result = word[Math.floor(number/10)] + " Puluh " + numberToWords(number - (Math.floor(number/10)*10));
+  } else if (number >= 100 && number < 200){
+    result = "Seratus" + numberToWords(number - 100);
+  } else if (number >= 200 && number < 1000){
+    result = word[Math.floor(number/100)] + " Ratus " + numberToWords(number - (Math.floor(number/100)*100));
+  } 
+  return result;
 }
 
 // Driver code
-console.log(numberToWords(1000000));
+console.log(numberToWords(197));
 
 module.exports = {
   numberToWords: numberToWords
