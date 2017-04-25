1a7f045ec6e6f4970186b3e011a81dd4dc9b8609
fajarkarim <fajarabdulkarim@gmail.com>
2017-04-25 20:45:18 +0700

    kurang ekspresi seratus, seribu, dll

--- number_in_words.js
+++ number_in_words.js
@@ -1,9 +1,22 @@
 function numberToWords(number) {
   // Your code here
+  let arrWords = ["", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan","sembilan"]
+  let arrSatuan = ["", "puluh", "ratus"];
+  let numStrArr = number.toString().split("");
+
+  if (numStrArr.length == 1) {
+    return arrWords[number];
+  } else {
+    let num = arrWords[parseInt(numStrArr[0])];
+    let satuan = arrSatuan[numStrArr.length - 1];
+    numStrArr.splice(0, 1);
+    let updateNum = parseInt(numStrArr.join(""));
+    return num + " " + satuan + " " + numberToWords(updateNum);
+  }
 }
 
 // Driver code
-console.log(numberToWords(1000000));
+console.log(numberToWords(999));
 
 module.exports = {
   numberToWords: numberToWords
