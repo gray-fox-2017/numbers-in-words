5771204a1a9fbbc199d00e9025551ba3336657b5
Hacktiv8 <hacktiv8-11@Hacktiv8-11.local>
2017-04-25 19:37:39 +0700

    kode untuk menyebutkan angka sampai 999

--- number_in_words.js
+++ number_in_words.js
@@ -1,9 +1,37 @@
 function numberToWords(number) {
   // Your code here
+  var bilangan = ['nol', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas']
+  var hasil = [];
+
+
+function penyebutAngka(num) {
+  if (2 > num/10 >= 1) {
+    hasil.push(bilangan[num%10]);
+    hasil.push('belas');
+  } else if (num/10 >= 2 && num/10 < 10 ) {
+    hasil.push(bilangan[Math.floor(num/10)]);
+    hasil.push('puluh');
+    hasil.push(bilangan[num%10]);
+  } else if (num/10 >= 10 && num/10 < 100) {
+    hasil.push(bilangan[Math.floor(num/100)])
+    hasil.push('ratus');
+    return penyebutAngka(num%100)
+  }
+  return hasil.join(' ')
+}
+
+
+  if (number<=11) {
+    return bilangan[number];
+  } else {
+    return penyebutAngka(number);
+  }
+
+
 }
 
 // Driver code
-console.log(numberToWords(1000000));
+console.log(numberToWords(717));
 
 module.exports = {
   numberToWords: numberToWords
